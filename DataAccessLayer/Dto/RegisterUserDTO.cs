90de0a247234d2a70f4d765a84ada9e4946835c0
Tom <tgadacy@gmail.com>
2023-06-19 13:12:32 -0400

    Add UserDTO.cs

---
+++ DataAccessLayer/Dto/RegisterUserDTO.cs
@@ -0,0 +1,8 @@
+namespace DataAccessLayer;
+
+public class RegisterUserDTO
+{
+    public string Username { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string Password { get; set; } = null!;
+}
